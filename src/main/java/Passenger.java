9bb5bdb476af8b8231142beb635a3d5efd71f649
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-11 10:48:18 +0000

    Passenger can return name

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,16 @@
+import java.util.HashMap;
+
 public class Passenger {
+
+    private String name;
+    private int numOfBags;
+
+    public Passenger(String name, int numOfBags) {
+        this.name = name;
+        this.numOfBags = numOfBags;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("Jimothy Burberidge", 2);
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Jimothy Burberidge", passenger.getName());
+    }
+
 }
