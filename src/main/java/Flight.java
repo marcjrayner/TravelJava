2384d2ce5d8cca34c27da81b53399403bc9b9221
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-11 11:09:41 +0000

    adds tests for flight, flight can get its plane

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Flight {
+    private Plane plane;
+    private ArrayList<Passenger> passengers;
+    private String flightNo;
+    private String destination;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(Plane plane, String flightNo, String destination, String departureAirport, String departureTime) {
+        this.plane = plane;
+        this.flightNo = flightNo;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+
+    public Plane getPlane() {
+        return this.plane;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    Flight flight;
+    Plane plane1;
+    Passenger passenger1;
+
+    @Before
+    public void before() {
+        plane1 = new Plane(PlaneType.DREAMLINER);
+        passenger1 = new Passenger("Jon", 1);
+        flight = new Flight(plane1, "EZ123", "EDI", "VAN", "14:00");
+    }
+
+    @Test
+    public void canGetPlane() {
+        assertEquals(plane1, flight.getPlane());
+    }
+//
+//    @Test
+//    public void canGetFlightNo() {
+//        assertEquals("EZ123", flight.getFlightNo());
+//
+//    }
+//
+//    @Test
+//    public void canGetDepartureAirport() {
+//        assertEquals("EDI", flight.getDepartureAirport());
+//
+//    }
+//
+//    @Test
+//    public void canGetDestination() {
+//        assertEquals("VAN", flight.getDestination());
+//
+//    }
+//
+//    @Test
+//    public void canGetDepartureTime() {
+//        assertEquals("14:00", flight.getDepartureTime());
+//
+//    }
+
 }
