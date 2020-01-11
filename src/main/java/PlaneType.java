d37277090371a54ee4fed9d3331997e762a0ea49
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-11 10:19:42 +0000

    adds PlaneType enum for plane

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,21 @@
 public enum PlaneType {
+    BOEING747(600, 15000),
+    DREAMLINER(450, 12000),
+    AIRBUSA380(900, 20000);
+
+    private final int capacity;
+    private final int maxWeight;
+
+    PlaneType(int capacity, int maxWeight) {
+        this.capacity = capacity;
+        this.maxWeight = maxWeight;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public int getMaxWeight() {
+        return maxWeight;
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+    Plane plane1;
+    Plane plane2;
+
+    @Before
+    public void before() {
+        plane1 = new Plane(PlaneType.BOEING747);
+        plane2 = new Plane(PlaneType.DREAMLINER);
+    }
+
+    @Test
+    public void canGetPlaneType() {
+        assertEquals(PlaneType.DREAMLINER, plane1.getPlaneType());
+    }
+
 }
